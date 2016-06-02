00f86e6ab69f9b0e767cfd312849fda754c308bd
lisamarie616 <lisamarie616@gmail.com>
2016-06-01 17:20:38 -0700

    update db

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160510034134) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "beach_downvotes", force: :cascade do |t|
     t.integer  "beach_id"
     t.integer  "user_id"
@@ -107,8 +110,8 @@ ActiveRecord::Schema.define(version: 20160510034134) do
     t.datetime "updated_at",                          null: false
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   create_table "visits", force: :cascade do |t|
     t.integer  "beach_id"
